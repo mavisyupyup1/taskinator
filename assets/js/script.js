c395a2653a9a67f6631ae65789573e6a40a43960
Your Name <graceliu@gracelius-iMac.home>
2021-10-20 11:55:44 -0400

    not accepting empty value and reset form

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@ var createTaskHandler = function(event){
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // check if input values are empty strings
+    if(!taskNameInput||!taskTypeInput){
+        alert("You need to fill out the task form!");
+        return false;
+    }
     //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -21,6 +26,8 @@ var createTaskHandler = function(event){
     //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
     console.dir(listItemEl);
+    formEl.reset();
+    
 };
 
 formEl.addEventListener("submit",createTaskHandler);
